a51bf30e18c463478cc3fc5c70e47295a66ec5e5
kmalio98 murillo conrado <kannermc@gmail.com>
2022-09-15 10:23:37 -0500

    fix: modifique archivo persona.h

--- Persona.h
+++ Persona.h
@@ -5,4 +5,28 @@
 #ifndef TAREA_HUESPEDES_TOROS_NEGROS_PERSONA_H
 #define TAREA_HUESPEDES_TOROS_NEGROS_PERSONA_H
 
+#include<cstdio>
+#include<iostream>
+#include<string>
+
+using std::string;
+
+class Persona{
+
+    private:
+
+        long id;
+        string name;
+        string sex;
+        int points;
+
+    public:
+
+        Persona();
+        Persona(long id2, string name2, string sex2, int points2);
+
+        void mostrarPersona();
+
+};
+
 #endif //TAREA_HUESPEDES_TOROS_NEGROS_PERSONA_H
