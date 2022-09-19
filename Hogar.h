58be634ce950c270fba48152c03b86f37f9c2de0
JuanCamiloDorado <doradocamiloj@gmail.com>
2022-09-18 21:32:19 -0500

    MOD: Modifique funcionalidades del archivo Hogar.h

--- Hogar.h
+++ Hogar.h
@@ -5,35 +5,36 @@
 #define TAREA_HUESPEDES_TOROS_NEGROS_HOGAR_H
 
 #include<cstdio>
+#include<iostream>
 #include<string>
 
 using std::string;
 
-class Hogar(){
+class Hogar{
 
 private:
 
-string direccionHogar;
-int numeroCamasDisponibles;
-bool alojarBebes;
-string descripcionHogar;
+    string direccionHogar;
+    int numeroCamasDisponibles;
+    bool alojarBebes;
+    string descripcionHogar;
 
 public:
 
-Hogar();
-Hogar(strgin direccion, int numeroCamas, bool alojaBebes2, string descripcion);
+    Hogar();
+    Hogar(string direccion, int numeroCamas, bool alojaBebes2, string descripcion);
 
-void mostrarHogar();
+    void mostrarHogar();
 
-string getDireccion() const;
-int getNumeroCamas() const;
-bool getAlojaBebes() const;
-string getDescripcion() const;
+    string getDireccion() const;
+    int getNumeroCamas() const;
+    bool getAlojaBebes() const;
+    string getDescripcion() const;
 
-void setDireccion();
-void setNumeroCamas();
-void setAlojaBebes();
-void setDescripcion();
+    void setDireccion(string direccion);
+    void setNumeroCamas(int numeroCamas);
+    void setAlojaBebes(bool alojarBebes);
+    void setDescripcion(string descripcion);
 
 };
 #endif //TAREA_HUESPEDES_TOROS_NEGROS_HOGAR_H
