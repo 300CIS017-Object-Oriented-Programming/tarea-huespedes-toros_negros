50eb43b3fc56d626bf35ff6ef79e96e95a602b6f
kmalio98 murillo conrado <kannermc@gmail.com>
2022-09-19 16:45:04 -0500

    MOD: agregar prototipos al Huesped.h, heredando metodos de la clase persona

--- Huesped.h
+++ Huesped.h
@@ -5,4 +5,21 @@
 #ifndef TAREA_HUESPEDES_TOROS_NEGROS_HUESPED_H
 #define TAREA_HUESPEDES_TOROS_NEGROS_HUESPED_H
 
+#include "Persona.h"
+
+class Huesped : public Persona{
+
+    private:
+
+        string infoClinica, ciudadFamiliar;
+
+    public:
+
+        string &getInfoClinica() const;
+        string &getCiudadFamiliar() const;
+
+        void setInfoClinica(const string);
+        void setCiudadFamiliar(const string);
+};
+
 #endif //TAREA_HUESPEDES_TOROS_NEGROS_HUESPED_H
