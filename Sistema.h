2487e1ecd583def689b83623f9f7857fcd222365
kaner918 <kannermc@gmail.com>
2022-09-20 08:46:42 -0500

    NEW: sistema.h

--- Sistema.h
+++ Sistema.h
@@ -5,4 +5,26 @@
 #ifndef TAREA_HUESPEDES_TOROS_NEGROS_SISTEMA_H
 #define TAREA_HUESPEDES_TOROS_NEGROS_SISTEMA_H
 
+#include<unordered_map>
+#include<vector>
+
+using std::vector;
+using std::unordered_map;
+
+class Sistema{
+
+    private:
+
+        unordered_map<int, Persona*> personas;
+        unordered_Map<string, Hogar*> hogares;
+        vector<Reserva*> reservas;
+        vector<Evaluacion*> evaluaciones;
+
+    private:
+
+        void agregarEvaluaciones();
+        void agregarPropietario();
+        void crearReservas();
+
+};
 #endif //TAREA_HUESPEDES_TOROS_NEGROS_SISTEMA_H
