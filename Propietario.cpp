a02d4689c96fc22e7514817de63e400da4cd9417
JuanCamiloDorado <doradocamiloj@gmail.com>
2022-09-19 23:25:02 -0500

    NEW: agregue el archivo.cpp del propietario herencia de personas

--- Propietario.cpp
+++ Propietario.cpp
@@ -1,3 +1,44 @@
-//
-// Created by orduz on 15/09/2022.
-//
+
+#include "Propietario.h"
+
+Propietario::Propietario() {
+    *hogar = nullptr;
+    disponibilidadPropietario = false;
+}
+
+Propietario::Propietario(bool disponibilidad, Hogar *hogar2) {
+    disponibilidadPropietario = disponibilidad;
+    *hogar = hogar2;
+}
+
+void Propietario::mostrarPropietarios(){
+    mostrarPersona();
+    if(disponibilidadPropietario){
+        cout<<"Disponilibidad: Si"<<endl;
+    }
+    else{
+        cout<<"Disponilibidad: No"<<endl;
+    }
+}
+
+bool Propietario::getDisponibilidadPropietario(){
+    return disponibilidadPropietario;
+}
+
+void Propietario::setDisponibilidadPropietario(const bool  disponibilidad){
+    disponibilidadPropietario = disponibilidad;
+}
+
+void Propietario::setHogar(const Hogar *hogar2) {
+    hogar = hogar2;
+}
+
+void Propietario::cambiarEstado() {
+    if(disponibilidadPropietario){
+        disponibilidadPropietario = false;
+    }
+
+    else{
+        disponibilidadPropietario = true;
+    }
+}
